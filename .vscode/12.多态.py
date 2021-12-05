f20e99ea31c7eb7e849de3ca6af084a4d63977fa
sige <837337164@qq.com>
2021-12-05 21:44:53 +0800

    20211205

--- .vscode/12.多态.py
+++ .vscode/12.多态.py
@@ -2,3 +2,37 @@
 # 多态从字面上理解是多种形态
 # 狗（狼狗，藏獒，哈士奇，古牧。。。）
 # 一个对象可以以不同的形态去呈现
+class A:
+    def __init__(self,name):
+        self._name = name  
+
+    @property
+    def  name(self):
+        return  self._name 
+    
+    @name.setter
+    def  name(self,name):
+        self._name = name 
+
+class B:
+    def __init__(self,name):
+        self._name = name  
+
+    @property
+    def  name(self):
+        return  self._name 
+    
+    @name.setter
+    def  name(self,name):
+        self._name = name 
+class C:
+    pass
+
+a = A('孙悟空')
+b = B('猪八戒')
+c = C()
+#定义一个函数  
+def  say_hello(obj):
+    print('你好 %s'%obj.name)
+
+say_hello(b)
