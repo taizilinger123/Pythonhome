13e011f6c63abe0b26a97cc07cf031dac61733dc
sige <837337164@qq.com>
2021-12-08 21:58:05 +0800

    20211208

--- .vscode/12.多态.py
+++ .vscode/12.多态.py
@@ -17,6 +17,9 @@ class A:
 class B:
     def __init__(self,name):
         self._name = name  
+    
+    def __len__(self):
+        return 10
 
     @property
     def  name(self):
@@ -37,4 +40,35 @@ c = C()
 def  say_hello(obj):
     print('你好 %s'%obj.name)
 
-say_hello(b)
+# 在say_hello_2中我们做了一个类型检查，也就是只有obj是A类型的对象时，才可以正常使用，
+#   其他类型的对象都无法使用该函数，这个函数就违反了多态
+# 违反了多态的函数，只适用于一种类型的对象，无法处理其他类型对象，这样导致函数的适应性非常的差
+# 注意，象isinstance()这种函数，在开发中一般是不会使用的！
+def  say_hello_2(obj):
+    # 做类型检查
+    if  isinstance(obj, A):
+        print('你好 %s'%obj.name)
+
+# say_hello(b)
+# say_hello_2(a)
+
+# 鸭子类型
+# 如果一个东西，走路像鸭子，叫声像鸭子，那么它就是鸭子
+
+# len()
+# 之所以一个对象能通过len()来获取长度，是因为对象中具有一个特殊方法__len__
+# 换句话说，只要对象中具有__len__特殊方法，就可以通过len()来获取它的长度
+l = [1,2,3]
+s = 'hello'
+# print(len(s))
+# print(len(l))
+print(len(b))
+print(len(c))
+
+# 面向对象的三大特征:
+#    封装
+#         - 确保对象中的数据安全
+#    继承
+#         - 保证了对象的可扩展性
+#    多态
+#         - 保证了程序的灵活性
