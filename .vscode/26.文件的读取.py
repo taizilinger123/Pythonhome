8b576f2e6fffebb9d98f2b53a9155203c96614de
sige <837337164@qq.com>
2022-01-03 22:33:41 +0800

    20220103

--- .vscode/26.文件的读取.py
+++ .vscode/26.文件的读取.py
@@ -14,10 +14,45 @@ try:
         # help(file_obj.read) 
         # read()可以接收一个size作为参数，该参数用来指定要读取的字符的数量
         #   默认值为-1，它会读取文件中的所有字符
+        # 可以为size指定一个值，这样read()会读取指定数量的字符
+        #     每一次读取都是从上次读取到位置开始读取的
+        #     如果字符的数量小于size,则会读取剩余所有的
+        #     如果已经读取到了文件的最后了，则会返回''空串
         # content = file_obj.read(-1)
         content = file_obj.read(6)
-        print(content)
-        print(len(content))
-
+        content = file_obj.read(6)
+        content = file_obj.read(6)
+        content = file_obj.read(6)
+        content = file_obj.read(6)
+        # print(content)
+        # print(len(content))
 except FileNotFoundError:
     print(f'{file_name} 这个文件不存在!')
+
+# 读取大文件的方式
+file_name =  r'c:\TestPython\.vscode\demo.txt'
+
+try:
+    with open(file_name,encoding='utf-8') as file_obj:
+        # 定义一个变量，来保存文件的内容
+        file_content = '' 
+        # 定义一个变量，来指定每次读取的大小
+        chunk = 100  
+        # 创建一个循环来读取文件的内容
+        while True:
+            # 读取chunk大小的内容
+            content = file_obj.read(chunk)
+
+            # 检查是否读取到了内容
+            if not content:
+                # 内容读取完毕，退出循环
+                break 
+            
+            # 输出内容
+            # print(content, end='')
+            file_content += content
+         
+except  FileNotFoundError:
+    print(f'{file_name} 这个文件不存在!')
+
+print(file_content)
